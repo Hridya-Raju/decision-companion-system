695ab09bc58ad13174771616049f9c466c9da90d
Hridya-Raju <hridya2701@gmail.com>
2026-03-03 00:37:51 +0530

    Auto commit

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const decisionRouter = require("./router/decisionRouter");
-
+const decisionRouter = require("./backend/router/decisionRouter");
+const path = require("path");
 const app = express();
 const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/decision", decisionRouter);
-
-app.get("/", (req, res) => {
-  res.send("Decision Companion API running");
-});
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
